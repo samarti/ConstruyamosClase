e0e5354872f71201bfa6960f19e15bda3145a4cc
samarti <samarti@uc.cl>
2016-05-04 20:09:32 -0300

    Added side menu items

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,22 +6,5 @@
 //= require bootstrap/alert
 //= require bootstrap/dropdown
 
-//= require_tree ../../../vendor/assets/javascripts/bower_components/moment/min/.
-//= require_tree ../../../vendor/assets/javascripts/bower_components/matchHeight/.
-//= require_tree ../../../vendor/assets/javascripts/bower_components/eonasdan-bootstrap-datetimepicker/build/js/.
-//= require_tree ../../../vendor/assets/javascripts/bower_components/jquery-typeahead/dist/.
-//= require_tree ../../../vendor/assets/javascripts/bower_components/html5-form-validation/dist/.
-//= require_tree ../../../vendor/assets/javascripts/bower_components/bootstrap-sweetalert/lib/.
-//= require_tree ../../../vendor/assets/javascripts/bower_components/autosize/dist/.
-//= require_tree ../../../vendor/assets/javascripts/bower_components/slick-carousel/slick/.
-//= require_tree ../../../vendor/assets/javascripts/bower_components/bootstrap-touchspin/dist/.
-//= require_tree ../../../vendor/assets/javascripts/bower_components/remarkable-bootstrap-notify/dist/.
-//= require_tree ../../../vendor/assets/javascripts/bower_components/pnotify/dist/.
-//= require_tree ../../../vendor/assets/javascripts/bower_components/blueimp-file-upload/js/.
-//= require_tree ../../../vendor/assets/javascripts/bower_components/blueimp-tmpl/js/.
-//= require_tree ../../../vendor/assets/javascripts/bower_components/blueimp-load-image/js/.
-//= require_tree ../../../vendor/assets/javascripts/bower_components/blueimp-canvas-to-blob/js/.
-//= require_tree ../../../vendor/assets/javascripts/bower_components/blueimp-file-upload/js/.
-
 //= require ./plugins2.js
 //= require ./app/app.js
