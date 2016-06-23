180b7053d3689ef5a94c799519a1fb8e50cf2527
Javier Diaz <javierdh9@gmail.com>
2016-06-23 17:02:18 -0400

    borrar documents

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -101,7 +101,7 @@ class DocumentsController < ApplicationController
   def destroy
     @document.destroy
     respond_to do |format|
-      format.html { redirect_to documents_url, notice: 'Document was successfully destroyed.' }
+      format.html { redirect_to current_teacher, notice: 'Document was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
