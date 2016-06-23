cef2056b63bfb5b009e7eb0a59f1320742a66626
samarti <samarti@uc.cl>
2016-06-23 17:57:54 -0300

    Added welcoming page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,15 @@
 class WelcomeController < ApplicationController
-  def index
-  end
+    def index
+        if logged_in?
+            id = current_teacher.id
+            @last_comments_aux = Document.all.select{|doc| doc[:teacher_id] == id}.collect{|x| x.ratings}.sort! {|a,b| a.created_at <=> b.created_at}[0]
+            if !@last_comments_aux.nil?
+                @last_received_comments = @last_comments_aux.take(3)
+            else
+                @last_received_comments = []
+            end
+        end
+    end
+
+
 end
